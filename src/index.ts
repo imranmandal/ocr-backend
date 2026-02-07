9d7a5d76c1ec634e5fa2e75d23ee1708dca4af98
imranmandal <imranmandal1888@gmail.com>
2026-02-07 06:13:01 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import multer from "multer";
+import Tesseract from "tesseract.js";
+
+const app = express();
+app.use(cors());
+const upload = multer({ dest: "uploads/" });
+
+app.get("/", (req, res) => {
+  return res.send("Hola amigo!");
+});
+
+app.post(
+  "/analyze-image",
+  upload.single("image"),
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.file) throw new Error("file missing");
+      const { data } = await Tesseract.recognize(req.file.path, "eng");
+
+      res.json({
+        text: data.text.trim(),
+      });
+    } catch (err) {
+      res.status(500).json({ error: "OCR failed" });
+    }
+  },
+);
+
+app.listen(4000, () => {
+    console.log("Server listening on port 4000")
+});
