a2134947bd8ae32d2b634da370bad796e94d00b6
imranmandal <imranmandal1888@gmail.com>
2026-02-07 06:24:00 +0530

    build fixed

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,6 @@ app.get("/", (req, res) => {
   return res.send("Hola amigo!");
 });
 
-function getAverageConfidence(data: Tesseract.Page) {
-  const words = data.words || [];
-  if (words.length === 0) return 0;
-
-  const sum = words.reduce((a, w) => a + (w.confidence || 0), 0);
-  return sum / words.length;
-}
-function isImageClear(data: Tesseract.Page) {
-  const avgConf = getAverageConfidence(data);
-  const text = data.text || "";
-
-  const validLines = text
-    .split("\n")
-    .filter((l) => /[a-zA-Z]/.test(l) && /\d/.test(l));
-
-  return {
-    clear: avgConf >= 70 && validLines.length >= 2,
-    confidence: Math.round(avgConf),
-    validLines: validLines.length,
-  };
-}
-
 app.post(
   "/analyze-image",
   upload.single("image"),
